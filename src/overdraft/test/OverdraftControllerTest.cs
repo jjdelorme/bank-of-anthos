009a7a1ba10980602f666093726c9f3ebb3138ce
Jason De Lorme <delormej@gmail.com>
2020-10-27 11:50:34 -0400

    Refactoring dir structure to add test project.

---
+++ src/overdraft/test/OverdraftControllerTest.cs
@@ -0,0 +1,15 @@
+using System;
+using Anthos.Samples.BankOfAnthos.Overdraft;
+using Xunit;
+
+namespace Anthos.Samples.BankOfAnthos.Overdraft.Tests
+{
+    public class OverdraftControllerTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
