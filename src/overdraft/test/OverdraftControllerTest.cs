ac850e6109647d6dd13202f8c83f98e724cb4614
Jason De Lorme <delormej@gmail.com>
2020-10-27 12:09:34 -0400

    Dummy test.

--- src/overdraft/test/OverdraftControllerTest.cs
+++ src/overdraft/test/OverdraftControllerTest.cs
@@ -6,10 +6,26 @@ namespace Anthos.Samples.BankOfAnthos.Overdraft.Tests
 {
     public class OverdraftControllerTest
     {
+        static OverdraftController controller = new OverdraftController(null);
+     
         [Fact]
-        public void Test1()
+        public void Version()
         {
-
+            var version = typeof(OverdraftController).Assembly.GetName().Version;
+            Assert.Equal(version, controller.Version());
         }
+
+        [Fact]
+        public void Ready()
+        {
+            Assert.True(controller.Ready() is Microsoft.AspNetCore.Mvc.OkObjectResult);
+        }
+
+        [Fact]
+        public void Create()
+        {
+            // Dummy data for now.
+            Assert.True(controller.Create() == "ACCOUNT_XXXX", "Should return account id");
+        }   
     }
 }
