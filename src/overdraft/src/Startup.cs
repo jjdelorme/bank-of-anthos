b6783d6475cd8b6152e0ecd0c63f98e5cde001ae
Jason De Lorme <delormej@gmail.com>
2020-10-27 20:38:40 -0400

    Adding JwtAuth framework.

--- src/overdraft/src/Startup.cs
+++ src/overdraft/src/Startup.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Text;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -6,11 +8,15 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace Anthos.Samples.BankOfAnthos.Overdraft
 {
     public class Startup
     {
+        private const string JWT_SECRET_NAME = "JwtSecret";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -29,6 +35,38 @@ namespace Anthos.Samples.BankOfAnthos.Overdraft
                     Title = "Anthos.Samples.BankOfAnthos.Overdraft", 
                     Version = "v1" });
             });
+
+            ConfigureJwtAuth(services);
+        }
+
+        private void ConfigureJwtAuth(IServiceCollection services)
+        {
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(GetJwtKey()),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });                        
+        }
+
+        private byte[] GetJwtKey()
+        {
+            string key = Configuration[JWT_SECRET_NAME];
+            if (string.IsNullOrEmpty(key))
+                throw new ApplicationException($"Missing JWT Key: {JWT_SECRET_NAME}");
+
+            return Encoding.ASCII.GetBytes(key);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
